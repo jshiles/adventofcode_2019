d2d6084656b3741ae87d9c1fb9e7a7456b94d119
John Shiles <john.shiles@talentsconnect.com>
2022-09-29 08:16:25 +0200

    completed day 04.

---
+++ adventofcode_2019/src/bin/day_03.rs
@@ -0,0 +1,149 @@
+/* 
+https://adventofcode.com/2019/day/3
+john shiles 
+*/
+
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use array_tool::vec::Intersect;
+use std::fmt;
+
+#[derive(Copy, Clone, PartialEq)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+pub trait Add {
+    fn add(&self, instr: &str) -> Result<Vec<Point>, Error>;
+}
+
+pub trait Manh {
+    fn manh(&self) -> i32;
+}
+
+impl Add for Point {
+    fn add(&self, instr: &str) -> Result<Vec<Point>, Error> {
+        let mut path: Vec<Point> = vec![];
+        let mut chars = instr.chars();
+        let direction = chars.next();
+        let magnitude: i32 = chars.as_str().parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        for i in 1..magnitude+1 {
+            let new_point: Point = match direction {
+                Some('R') => Point { x: self.x+i, y: self.y, },
+                Some('L') => Point { x: self.x-i, y: self.y, },
+                Some('U') => Point { x: self.x, y: self.y+i, },
+                Some('D') => Point { x: self.x, y: self.y-i, },
+                _other => panic!("Error obtaining direction {:?}", direction)
+            };
+            path.push(new_point);
+        }   
+        Ok(path)
+    }
+}
+
+impl Manh for Point {
+    fn manh(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({:?}, {:?})", &self.x, &self.y)
+    }
+}
+
+fn read_input(path: &str) -> Result<(String, String), Error> {   
+    let file = File::open(path)?;
+    let mut br = BufReader::new(file);
+
+    let mut path1 = String::new();
+    br.read_line(&mut path1).expect("read_line error");
+
+    let mut path2 = String::new();
+    br.read_line(&mut path2).expect("read_line error");
+
+    Ok((path1, path2))
+}
+
+fn construct_path(input: &str) -> Vec<Point> {
+    let start: Point = Point { x: 0, y: 0, };
+    let mut path: Vec<Point> = vec![start];
+
+    let parts: Vec<&str> = input.trim().split(",").collect();
+    for part in parts {
+        let mut additional_path = match path.last().unwrap().add(&part) {
+            Ok(p) => p,
+            Err(e) =>  panic!("Error obtaining project root {:?}", e),
+        };
+        // println!("{}", additional_path.last().unwrap());
+        path.append(&mut additional_path);
+    }
+
+    path
+}
+
+fn find_closest_intersection_distance(input1: &str, input2: &str) -> i32 {
+    let path_1: Vec<Point> = construct_path(input1);
+    let path_2: Vec<Point> = construct_path(input2);
+
+    let mut lowest = 0;
+    for p in path_1.intersect(path_2) {
+        // println!("Intersection point: {}, Man Distance: {:?}", p, p.manh());
+        if lowest == 0 || p.manh() < lowest {
+            lowest = p.manh();
+        }
+    }
+    println!("Lowest intersection: {:?}", lowest);
+    lowest
+}
+
+fn find_closest_intersection_steps(input1: &str, input2: &str) -> i32 {
+    let path_1: Vec<Point> = construct_path(input1);
+    let path_2: Vec<Point> = construct_path(input2);
+
+    let mut lowest = 0;
+    for p in path_1.intersect(path_2.clone()) {
+        let index_1 = path_1.iter().position(|&r| r == p).unwrap();
+        let index_2 = path_2.iter().position(|&r| r == p).unwrap();
+        if lowest == 0 || index_1+index_2 < lowest {
+            lowest = index_1+index_2;
+        }
+    }
+    println!("Lowest steps to intersection: {:?}", lowest);
+    lowest as i32
+}
+
+
+
+fn main () {
+    let test_input_1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+    let test_input_2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+    assert_eq!(
+        find_closest_intersection_distance(&test_input_1, &test_input_2),
+        159);
+
+    let project_root_dir = match project_root::get_project_root() {
+        Ok(p) => p,
+        Err(e) =>  panic!("Error obtaining project root {:?}", e)
+    };
+    let input_data_file = format!("{}{}", 
+        project_root_dir.into_os_string().into_string().unwrap(), 
+        "/data/day_03.txt");
+
+    /*
+    Part 1: find closest intersection by manhatten distance.
+    */
+    let (input_1, input_2) = read_input(&input_data_file).unwrap();
+    let p1_answer = find_closest_intersection_distance(&input_1, &input_2);
+    println!("{:?}", p1_answer);
+
+    /*
+    Part 2: find closest intersection by sum of steps.
+    */
+    let (input_1_p2, input_2_p2) = read_input(&input_data_file).unwrap();
+    let p2_answer = find_closest_intersection_steps(&input_1_p2, &input_2_p2);
+    println!("{:?}", p2_answer);
+
+}
---
+++ adventofcode_2019/src/bin/day_04.rs
@@ -0,0 +1,96 @@
+/* 
+https://adventofcode.com/2019/day/4
+john shiles 
+*/
+
+use itertools::Itertools;
+
+fn number_to_vec(n: u32) -> Vec<u32> {
+    n.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
+}
+
+fn valid_password(password: u32) -> bool {
+    /*
+    Check if there is at least two consecutive numbers AND each number is 
+    greater than or equal to the prior number.
+    */
+    let mut consecutive_found: bool = false;
+    let password_vec = number_to_vec(password);
+
+    for (i, j) in (0..password_vec.len()).tuple_windows() {
+        if  &password_vec[i as usize] == &password_vec[j as usize] {
+            consecutive_found = true;
+        }
+        if &password_vec[i as usize] > &password_vec[j as usize] {
+            return false
+        }
+    }
+    return consecutive_found
+}
+
+fn valid_password_extended(password: u32) -> bool {
+    /*
+    If valid_password is true, then apply an additional rule: check if there 
+    is at least 1 pattern of exactly 2 consecutive numbers.
+    */
+    let mut valid: bool = false;
+    if valid_password(password) {
+        let password_vec = number_to_vec(password);
+        for (i, j, k, l) in (0..password_vec.len()).tuple_windows::<(_, _, _, _)>() {
+            if i == 0 && &password_vec[i as usize] == &password_vec[j as usize] && 
+                &password_vec[i as usize] != &password_vec[k as usize] {
+                // beginning of the list
+                valid = true;
+            }
+            else if l == password_vec.len()-1 && 
+                &password_vec[k as usize] == &password_vec[l as usize] && 
+                &password_vec[j as usize] != &password_vec[k as usize] {
+                // ending of the list
+                valid = true;
+            } else if &password_vec[i as usize] != &password_vec[j as usize] && 
+                &password_vec[j as usize] == &password_vec[k as usize] &&
+                &password_vec[k as usize] != &password_vec[l as usize] { 
+                    valid = true;
+            }
+        }
+    }
+    return valid
+}
+
+fn main() {
+    // Run some supplied tests
+    assert_eq!(true, valid_password(111111));
+    assert_eq!(false, valid_password(223450));
+    assert_eq!(false, valid_password(123789));
+    assert_eq!(false, valid_password_extended(111111));
+    assert_eq!(true, valid_password_extended(112233));
+    assert_eq!(false, valid_password_extended(123444));
+    assert_eq!(true, valid_password_extended(111122));
+
+    /*
+    part 1: How many valid passwords are there within the range 123257-647015
+    according the base logic?
+    */
+    let mut count = 0;
+    for i in 123257..647016 {
+        if valid_password(i) {
+            count = count + 1;
+        }
+    }
+    println!("Valid codes (part 1): {:?}", count); // 2220
+
+    /*
+    part 2: How many valid passwords are there within the range 123257-647015 
+    according to the extended logic?
+    */
+    count = 0;
+    for i in 123257..647016 {
+        if valid_password_extended(i) {
+            count = count + 1;
+        }
+    }
+    println!("Valid codes (part 2): {:?}", count); // 1515
+}
